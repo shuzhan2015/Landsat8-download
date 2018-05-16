24e37ab0877bb143e64b1b6448d2d0146d0be8e2
shuzhan2015 <ericvan2012@gmail.com>
2018-05-16 10:02:02 -0500

    update new download file

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ This python script can automate the work of downloading Landsat 8 data
 from Google Cloud Storage using Google's gsutil tool and then extract the bz file.
 You can read more about the Google Cloud Storage public datasets project for [Landsat Data][]. 
 
+Note: Beginning late 2017, USGS started to remove Landsat Pre-Collection data and archive Collection 1 data. The naming convention for the Landsat dataset was also changed. `download_landsat8_new.py` is the script you should run now, since the old `download_landsat8.py` will not work for the new change. 
+
 ## Prerequisites
 You need to have Ptyhon 2.7 installed. You also need to have the Google Cloud Storage `gsutil` command line tool 
 installed on your laptop or computer. If you are using Windows, the 
@@ -41,21 +43,21 @@ the installation instruction is:
  The date could be in the format yyyymmdd, yyyyjjj, yymmdd, yyjjj, or mmdd. So, you could run the script by
  passing the three arguments like this:
  ```sh
-  $ ./download_landsat8.py --path 22 --row 39 --date 20170401
+  $ ./download_landsat8_new.py --path 22 --row 39 --date 20170401
  ```
  Or, simply like this:
  ```sh
-  $ ./download_landsat8.py -p 22 -r 39 -d 20170401
+  $ ./download_landsat8_new.py -p 22 -r 39 -d 20170401
  ```
   Or, If the date argument is omitted, then the latest date available will be downloaded:
  ```sh
-  $ ./download_landsat8.py -p 22 -r 39
+  $ ./download_landsat8_new.py -p 22 -r 39
  ```
   Or, you can also specify the path and row in the config file. Change the path and row in 
   config file, do not put a zero before the path and row number: 
  ```sh
   $ vim config
-  $ ./download_landsat8.py -d 20170401
+  $ ./download_landsat8_new.py -d 20170401
  ```
   The downloaded file is in a compressed bz and tar format. The script will automatically 
   unzip the bz file and untar the tar file. It will also create the corresponding directory 
---
+++ download_landsat8_new.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+
+#Example Usage:
+#           ./download_landsat8.py --path 22 --row 39 --date 20170401
+# or        ./download_landsat8.py -p 22 -r 39 -d 20170401
+# or   if path and row are specified in the config file, then ./download_landsat8.py -d 20170401
+# or   if the date argument is omitted, the latest date available will be downloaded: ./download_landsat8.py -p 22 -r 39
+
+#Written by Shuzhan Fan on 05/29/2016
+#Updated by Shuzhan Fan on 05/15/2018
+
+import sys
+import os
+from ConfigParser import SafeConfigParser
+from datetime import datetime
+from subprocess import Popen, PIPE
+import re
+import logging
+import tarfile
+import glob
+import argparse
+
+def get_config(file_name):
+	parser = SafeConfigParser()
+	parser.read(file_name)
+	return parser
+
+def parse_path_row(path, row):
+	if len(path) == 2:
+		path = '0' + path
+	if len(row) == 2:
+		row = '0' + row
+	return path, row
+
+def search_latest(path, row):
+	'''
+		This method will search for the latest date available in the directory and return the latest date object.
+	'''
+	path, row = parse_path_row(path, row)
+	path_str = 'gs://gcp-public-data-landsat/LC08/01/' + path + '/' + row
+	p = Popen('gsutil ls '+path_str, stdout=PIPE, stderr=PIPE, shell=True)
+	stdout = p.stdout.read()
+	stderr = p.stderr.read()
+	if stderr:
+		logger.error(stderr)
+		sys.exit()
+	if stdout:
+		data_path_list = stdout.split()
+		data_path_latest = data_path_list[-1]           #e.g. gs://gcp-public-data-landsat/LC08/01/022/039/LC08_L1TP_022039_20180506_20180506_01_T1/
+		data_latest = data_path_latest.split('/')[-2]   #e.g. LC08_L1TP_022039_20180506_20180506_01_T1
+		date_str = data_latest.split('_')[3]            #e.g. 20180506
+		dobj = datetime.strptime(date_str, '%Y%m%d') 
+		return dobj
+
+def parse_date(date):
+	'''
+		This method will parse the user-specified date format
+		(yyyymmdd, yyyyjjj, yymmdd, yyjjj, or mmdd) and return
+		a date object.
+	'''
+	if len(date) == 8:
+		dobj = datetime.strptime(date, '%Y%m%d')
+	elif len(date) == 7:
+		dobj = datetime.strptime(date, '%Y%j')
+	elif len(date) == 6:
+		dobj = datetime.strptime(date,'%y%m%d')
+	elif len(date) == 5:
+		dobj = datetime.strptime(date,'%y%j')
+	elif len(date) == 4:
+		today = datetime.today()
+		year = str(today.year)
+		d = year + date
+		dobj = datetime.strptime(d,'%Y%m%d')
+	logger.info('    Date: %s' % (dobj.strftime('%Y-%m-%d')))
+	return dobj
+
+def check_date(path, row, dobj):
+	'''
+		This method will check the date user types. If the date matches one file
+		in the Google Cloud directory, then return the data path string. If not match,
+		user needs to type a different matched date.
+	'''
+	path, row = parse_path_row(path, row)
+
+	path_str = 'gs://gcp-public-data-landsat/LC08/01/' + path + '/' + row
+
+	p = Popen('gsutil ls '+path_str, stdout=PIPE, stderr=PIPE, shell=True)
+	stdout = p.stdout.read()
+	stderr = p.stderr.read()
+	if stderr:
+		logger.error(stderr)
+		sys.exit()
+	if stdout:
+		data_path_list = stdout.split()
+		date_str = dobj.strftime('%Y%m%d')
+		data_str = 'LC08_L1TP_' + path + row + '_' + date_str
+		regex = re.compile(data_str)
+                result = ''
+		for data in data_path_list:
+			if regex.search(data):
+                            if data[-3:-1] == 'RT':
+                                result = data
+                            if data[-3:-1] == 'T1':
+                                result = data 
+                if result:
+		    logger.info('%s_* is found in the directory.' % (data_str))
+		    return result  
+                else:
+                    logger.error("The data for the date you requested is not in the directory. Please type a different date!")
+		    sys.exit()
+
+def download(path, row, date):
+	path, row = parse_path_row(path, row)
+	logger.info('You are requesting:')
+	logger.info('    Path: %s     Row: %s' %(path, row))
+	dobj = parse_date(date)
+	data_path_str = check_date(path, row, dobj)
+	if not os.path.isdir('./data'):
+		os.mkdir('./data')
+	logger.info('Downloading the data...')
+	p = Popen('gsutil -m cp -r '+data_path_str+' ./data', stdout=PIPE, stderr=PIPE, shell=True)
+	stdout = p.stdout.read()
+	stderr = p.stderr.read()
+	if stderr:
+		logger.error(stderr)
+	if stdout:
+		logger.info(stdout)
+
+def parse_command_line():
+	DATE_str = '20000101'
+	parser = get_config('config')
+	path_config = parser.get('Path-Row','path')
+	row_config = parser.get('Path-Row','row')
+	path_default, row_default = parse_path_row(path_config, row_config)
+
+	parser_cml = argparse.ArgumentParser(description='Script to download Landsat8 frame for a path and row and a given date')
+	parser_cml.add_argument('-p','--path', type=str, help='The path of the frame to be downloaded', default=path_default)
+	parser_cml.add_argument('-r', '--row', type=str, help='The row of the frame to be downloaded', default=row_default)
+	parser_cml.add_argument('-d', '--date', type=str, help='The date of the frame to be downloaded', default=DATE_str)
+	args = parser_cml.parse_args()
+	path = args.path
+	row = args.row
+	DATE = search_latest(path, row)
+	DATE_str = datetime.strftime(DATE, '%Y%m%d')
+	parser_cml.set_defaults(date=DATE_str)
+	args = parser_cml.parse_args()
+
+	return args
+
+def logger():
+	logger = logging.getLogger()
+	logger.setLevel(logging.INFO)
+	handler = logging.StreamHandler()
+	handler.setLevel(logging.INFO)
+	formatter = logging.Formatter('%(message)s')
+	handler.setFormatter(formatter)
+	logger.addHandler(handler)
+	return logger
+
+if __name__ == '__main__':
+	logger = logger()
+
+	opts = parse_command_line()
+	path = opts.path
+	row = opts.row
+	date = opts.date
+
+	download(path, row, date)
+	logger.info('Download complete!\n')
